9b762eb16cc57bda2cfb3be46f48cd1284370306
UserDev <UserDev>
2019-03-27 23:37:46 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ex';
+export class AppComponent implements OnInit {
+
+  title = 'Tic Tac Toe';
+  player1: string;
+  player2: string;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+
 }
